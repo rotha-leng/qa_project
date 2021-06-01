8ae8c5768b1fb9c678e620b00fea4b4f51656f99
Rotha <rotha.morakot@gmail.com>
2021-06-01 20:40:33 +0700

    update init.py

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,9 @@ DB_USER = 'postgres'  # database user
 DB_PWD = '2001'  # database password
 DB_NAME = 'qadb'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gimsasfzkfvjfx:a05f9df17919b2925711a7923d52e8f09c370447bd83707cd1c23f33c84f0013@ec2-54-91-188-254.compute-1.amazonaws.com:5432/da80g0ijacfal4" % (DB_USER, DB_PWD, DB_NAME)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gimsasfzkfvjfx:a05f9df17919b2925711a7923d52e8f09c370447bd83707cd1c23f33c84f0013@ec2-54-91-188-254.compute-1.amazonaws.com:5432/da80g0ijacfal4" 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
