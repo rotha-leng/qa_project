fd6fbb7692c2767fddcda2812df58b96bb2bea83
Rotha <rotha.morakot@gmail.com>
2021-06-01 13:54:48 +0700

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-#from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin
+
+from flask import Flask, render_template, request, session, url_for, redirect, flash
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '!loveyou@ll'
@@ -12,11 +16,11 @@ DB_NAME = 'qadb'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://%s:%s@localhost:5432/%s" % (DB_USER, DB_PWD, DB_NAME)
 
-"""login_manager = LoginManager()
+login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = "login"
-"""
+
 
 db = SQLAlchemy(app)
 
@@ -25,3 +29,4 @@ from .question import *  # register post view, so that its url is routable
 @app.route('/')
 def index():
     return render_template('home/index.html')
+
