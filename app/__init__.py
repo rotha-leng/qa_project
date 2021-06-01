8353eb17b794e39157fd7805560044331de1030b
Rotha <rotha.morakot@gmail.com>
2021-06-01 17:21:30 +0700

    update init

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ DB_USER = 'postgres'  # database user
 DB_PWD = '2001'  # database password
 DB_NAME = 'qadb'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://kagfibsmgdasmu:fab3abd640196dd1d2392d5907c0c9280f0ff206269c254ffd08ddd1e4adc93f@ec2-34-195-143-54.compute-1.amazonaws.com:5432/ddjb62gavmr843" % (DB_USER, DB_PWD, DB_NAME)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gimsasfzkfvjfx:a05f9df17919b2925711a7923d52e8f09c370447bd83707cd1c23f33c84f0013@ec2-54-91-188-254.compute-1.amazonaws.com:5432/da80g0ijacfal4" % (DB_USER, DB_PWD, DB_NAME)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
